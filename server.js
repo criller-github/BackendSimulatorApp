a203250fd057afe9d04101ebef3d03cd24193fdc
“criller-github” <chri38nq@edu.zealand.dk>
2024-11-18 13:42:15 +0100

    server ændringringer

--- server.js
+++ server.js
@@ -10,23 +10,30 @@ app.use(cors());
 app.use(cors(corsOptions));*/
 const db = require("./app/models");
 
-db.sequelize.sync();
-
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+db.sequelize.sync() //sync() metoden opretter tabellerne i databasen, hvis de ikke allerede eksisterer
+  .then(() => { //hvis sync() metoden lykkedes
+    console.log('synced');
+  })
+  .catch((err) => { //hvis sync() metoden fejlede
+    console.log("Kunne ikke synkronisere med databasen: " + err.message);
+  });
+
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Velkommen til katte-app API" });
 });
 
-//inklere vores router
+//inklere vores router /med eller uden .js?/
 require("./app/routes/catText.routes.js")(app);
 
-require("./app/routes/tutorial.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
