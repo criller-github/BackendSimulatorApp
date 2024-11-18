0f07d70c9392838a950062fc3724652e0184fb38
“criller-github” <chri38nq@edu.zealand.dk>
2024-11-18 12:37:53 +0100

    ændret catText.model

--- app/models/catText.model.js
+++ app/models/catText.model.js
@@ -1,13 +1,15 @@
 module.exports = (sequelize, Sequelize) => {
-    const CatText = sequelize.define("catText", {
-      title: {
+    const CatText = sequelize.define("cat_text", {
+      id: {
+        type: Sequelize.STRING,
+        Autoincrement: true,
+        primaryKey: true
+      },
+      image_id: { //et id til at identificere hvilken kattekategori teksten hører til
         type: Sequelize.STRING
       },
-      description: {
-        type: Sequelize.STRING
-      },
-      published: {
-        type: Sequelize.BOOLEAN
+      text: { //teskten der skal vises i appen til brugeren
+        type: Sequelize.STRING //ændre til string for at gemme mere komplekse data end 'boolean' som er primært til simple sandt/falsk værdier
       }
     });
   
